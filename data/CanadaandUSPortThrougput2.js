5b472a42401aef0a9683776e2a0b203e5045f126
pieterdewaard1986 <pieterdewaard@hotmail.com>
2017-06-11 00:57:04 +0200

    Add files via upload

---
+++ data/CanadaandUSPortThrougput2.js
@@ -0,0 +1,19 @@
+var json_CanadaandUSPortThrougput2 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "PORT": " Charleston (SC) ", "x": -79.27539539, "y": 33.36324724, "TEU_2015": 1973204.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -79.27539539, 33.36324724 ] } },
+{ "type": "Feature", "properties": { "PORT": " Halifax (NS)", "x": -63.12311845, "y": 44.78109043, "TEU_2015": 418359.0, "COUNTRY": " Canada " }, "geometry": { "type": "Point", "coordinates": [ -63.12311845, 44.78109043 ] } },
+{ "type": "Feature", "properties": { "PORT": " Hampton Roads (VA) ", "x": -76.352854, "y": 36.873589, "TEU_2015": 2549271.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -76.352854, 36.873589 ] } },
+{ "type": "Feature", "properties": { "PORT": " Houston  (TX) ", "x": -95.21643942, "y": 29.67934519, "TEU_2015": 2130544.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -95.21643942, 29.67934519 ] } },
+{ "type": "Feature", "properties": { "PORT": " Los Angeles\/Long Beach (CA)", "x": -118.1837943, "y": 33.76635005, "TEU_2015": 15352524.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -118.1837943, 33.76635005 ] } },
+{ "type": "Feature", "properties": { "PORT": " Montreal (QU) ", "x": -72.54973936, "y": 46.35125511, "TEU_2015": 1446075.0, "COUNTRY": " Canada " }, "geometry": { "type": "Point", "coordinates": [ -72.54973936, 46.35125511 ] } },
+{ "type": "Feature", "properties": { "PORT": " New Orleans (LA) ", "x": -90.04545765, "y": 29.93845757, "TEU_2015": 524875.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -90.04545765, 29.93845757 ] } },
+{ "type": "Feature", "properties": { "PORT": " New York\/New Jersey ", "x": -74.01147114, "y": 40.65219104, "TEU_2015": 6371720.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -74.01147114, 40.65219104 ] } },
+{ "type": "Feature", "properties": { "PORT": " Oakland (CA) ", "x": -122.2669266, "y": 37.78188778, "TEU_2015": 2277521.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -122.26692660000001, 37.78188778 ] } },
+{ "type": "Feature", "properties": { "PORT": " Prince Rupert (BC) ", "x": -130.2829739, "y": 54.21650764, "TEU_2015": 776414.0, "COUNTRY": " Canada " }, "geometry": { "type": "Point", "coordinates": [ -130.2829739, 54.21650764 ] } },
+{ "type": "Feature", "properties": { "PORT": " Savannah (GA) ", "x": -80.7174121, "y": 32.16907715, "TEU_2015": 3737402.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -80.7174121, 32.16907715 ] } },
+{ "type": "Feature", "properties": { "PORT": " Seattle\/Tacoma Alliance (WA) ", "x": -122.4161979, "y": 47.27751144, "TEU_2015": 3529446.0, "COUNTRY": " United States " }, "geometry": { "type": "Point", "coordinates": [ -122.4161979, 47.27751144 ] } },
+{ "type": "Feature", "properties": { "PORT": " Vancouver (BC) ", "x": -122.246143, "y": 49.11253493, "TEU_2015": 3054468.0, "COUNTRY": " Canada " }, "geometry": { "type": "Point", "coordinates": [ -122.246143, 49.11253493 ] } }
+]
+}
